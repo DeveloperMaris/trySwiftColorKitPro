75a0a2637521bc88b94ec5ff4a5afb349a1fb9ee
Daniel Saidi <daniel.saidi@gmail.com>
2021-03-31 22:17:20 +0200

    Replace targets with a binary target

--- Package.swift
+++ Package.swift
@@ -22,11 +22,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "trySwiftColorKitPro",
-            dependencies: []),
-        .testTarget(
-            name: "trySwiftColorKitProTests",
-            dependencies: ["trySwiftColorKitPro"]),
+            path: "Sources/trySwiftColorKitPro.xcframework")
     ]
 )

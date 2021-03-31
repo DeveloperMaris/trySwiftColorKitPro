927a1c9b57de447c7f055059eacf7f2f55e416c4
Daniel Saidi <daniel.saidi@gmail.com>
2021-03-31 22:21:48 +0200

    Add dependency to core library

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
             targets: ["trySwiftColorKitPro"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/danielsaidi/trySwiftColorKit", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
